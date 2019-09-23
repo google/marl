7c3f380a907a6db9d3b38dca9cbda8952ba9337c
Ben Clayton <bclayton@google.com>
2019-09-23 02:00:15 +0100

    Comments: Improve & make style consistent

--- include/marl/memory.h
+++ include/marl/memory.h
@@ -29,9 +29,9 @@ inline T alignUp(T val, T alignment) {
   return alignment * ((val + alignment - 1) / alignment);
 }
 
-// aligned_malloc allocates size bytes of uninitialized storage with the minumum
-// specified byte alignment.
-// The pointer returned must be freed with aligned_free().
+// aligned_malloc() allocates size bytes of uninitialized storage with the
+// specified minimum byte alignment. The pointer returned must be freed with
+// aligned_free().
 inline void* aligned_malloc(size_t alignment, size_t size) {
   MARL_ASSERT(alignment < 256, "alignment must less than 256");
   auto allocation = new uint8_t[size + sizeof(uint8_t) + alignment];
@@ -44,7 +44,7 @@ inline void* aligned_malloc(size_t alignment, size_t size) {
   return aligned;
 }
 
-// aligned_free frees memory allocated by aligned_malloc.
+// aligned_free() frees memory allocated by aligned_malloc.
 inline void aligned_free(void* ptr) {
   auto aligned = reinterpret_cast<uint8_t*>(ptr);
   auto offset = aligned[-1];
@@ -52,7 +52,7 @@ inline void aligned_free(void* ptr) {
   delete[] allocation;
 }
 
-// aligned_new allocates and constructs an object of type T, respecting the
+// aligned_new() allocates and constructs an object of type T, respecting the
 // alignment of the type.
 // The pointer returned by aligned_new() must be deleted with aligned_delete().
 template <typename T, typename... ARGS>
@@ -69,7 +69,7 @@ void aligned_delete(T* object) {
   aligned_free(object);
 }
 
-// make_aligned_shared returns a new object wrapped in a std::shared_ptr that
+// make_aligned_shared() returns a new object wrapped in a std::shared_ptr that
 // respects the alignemnt of the type.
 template <typename T, typename... ARGS>
 inline std::shared_ptr<T> make_aligned_shared(ARGS&&... args) {
@@ -77,7 +77,7 @@ inline std::shared_ptr<T> make_aligned_shared(ARGS&&... args) {
   return std::shared_ptr<T>(ptr, aligned_delete<T>);
 }
 
-// aligned_storage is a replacement for std::aligned_storage that isn't busted
+// aligned_storage() is a replacement for std::aligned_storage that isn't busted
 // on older versions of MSVC.
 template <size_t SIZE, size_t ALIGNMENT>
 struct aligned_storage {
