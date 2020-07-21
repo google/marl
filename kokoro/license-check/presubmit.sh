1a28daf0d89361384bae38fd07a1f7e532eaad9d
Ben Clayton <bclayton@google.com>
2020-07-21 15:03:46 +0100

    Add license checker config and kokoro presubmit
    `license-checker` is a tool that verifies each file has contains a permitted license header.
    
    See https://github.com/ben-clayton/license-checker for more information.
    
    Also add missing licenses to presubmit scripts.

--- kokoro/license-check/presubmit.sh
+++ kokoro/license-check/presubmit.sh
@@ -16,16 +16,12 @@
 
 set -e # Fail on any error.
 
-ROOT_DIR=`pwd`
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd )"
+ROOT_DIR="$( cd "${SCRIPT_DIR}/../.." >/dev/null 2>&1 && pwd )"
 
 docker run --rm -i \
   --volume "${ROOT_DIR}:${ROOT_DIR}" \
   --volume "${KOKORO_ARTIFACTS_DIR}:/mnt/artifacts" \
   --workdir "${ROOT_DIR}" \
-  --env BUILD_SYSTEM=$BUILD_SYSTEM \
-  --env BUILD_TOOLCHAIN=$BUILD_TOOLCHAIN \
-  --env BUILD_TARGET_ARCH=$BUILD_TARGET_ARCH \
-  --env BUILD_SANITIZER=$BUILD_SANITIZER \
   --entrypoint "${SCRIPT_DIR}/presubmit-docker.sh" \
   "gcr.io/shaderc-build/radial-build:latest"
